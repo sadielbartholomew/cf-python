25e3acdee6790f93216c9cdb38043c25a1244721
Sadie L. Bartholomew <sadie.bartholomew@ncas.ac.uk>
2026-09-04 23:33:16 +0100

    Add missing optional deps, pyproj & healpix, to env output

--- cf/functions.py
+++ cf/functions.py
@@ -3,6 +3,7 @@ import platform
 import warnings
 from collections.abc import Iterable
 from functools import lru_cache, partial
+from importlib.metadata import version
 from importlib.util import find_spec
 from itertools import product
 from os import mkdir
@@ -3123,6 +3124,11 @@ def environment(display=True, paths=True):
         "cartopy": _get_module_info("cartopy", try_except=True),
         "cfplot": _get_module_info("cfplot", try_except=True),
         "cf": (__version__, _os_path_abspath(__file__)),
+        # Healpix module doesn't define a __version__, so can't as standard use
+        # _get_module_info, but I have opened an Issue with them to define
+        # this (see: TODO)
+        "healpix": (version("healpix"), find_spec("healpix").origin),
+        "pyproj": _get_module_info("pyproj", try_except=True),
     }
     string = "{0}: {1!s}"
     if paths:
