0a8d61620aa28016a5684ec70c4adf5a3f3d24ed
Sadie L. Bartholomew <sadie.bartholomew@ncas.ac.uk>
2026-09-08 20:54:18 +0100

    Update test_Data_concatenate: make log output test deterministic

--- cf/test/test_Data.py
+++ cf/test/test_Data.py
@@ -1101,10 +1101,11 @@ class DataTest(unittest.TestCase):
         # ...when joining along axis=0 (the default)
         self.assertEqual(d.cyclic(), {0, 1})
         with self.assertLogs(level=-1) as catch:
+            print("LOG OUTPUT", catch.output)
             f = cf.Data.concatenate([d, e])
             self.assertTrue(
                 any(
-                    "Concatenating along a cyclic axis (0)" in log_msg
+                    "Concatenating along a cyclic axis" in log_msg
                     for log_msg in catch.output
                 )
             )
